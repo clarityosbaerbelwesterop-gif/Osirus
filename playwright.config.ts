56e2fda90f6e8817200316b38097a9836cdec09f
clarityosbaerbelwesterop-gif <clarityos.baerbelwesterop@gmail.com>
2026-09-20 16:58:16 +0200

    ci: add quality and product smoke gates

---
+++ playwright.config.ts
@@ -0,0 +1,1 @@
+import {defineConfig} from "@playwright/test";export default defineConfig({testDir:"./e2e",use:{baseURL:process.env.PLAYWRIGHT_BASE_URL??"http://127.0.0.1:3000"},webServer:process.env.CI?{command:"npm run start",url:"http://127.0.0.1:3000",reuseExistingServer:false}:undefined});
